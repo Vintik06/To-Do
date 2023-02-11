5fce138e334993eadc23206b3ba70ad4ce9431b3
Vintik06 <oven.oleinik@gmail.com>
2023-02-11 11:39:07 +0200

    second commit

--- script.js
+++ script.js
@@ -8,6 +8,41 @@ let input = document.querySelector(".input");
 
 let todoList = document.querySelector(".todo-list");
 
+const allToDos = localStorage.getItem("to-dos");
+
+if(allToDos != null) {
+    const myArray = allToDos.split("|");
+    for (let index = 0; index < myArray.length; index++) {
+        if (myArray[index] != "") {
+                const para = document.createElement("p");
+                para.classList.add("par" + index);
+                para.id = "par" + index;
+                para.innerText = myArray[index];
+                const span = document.createElement("span");
+                span.classList.add("par" + index); 
+                span.classList.add("spans"); 
+                span.innerText = 'done';
+                todoList.appendChild(para);  
+                para.appendChild(span);   
+            }
+        }
+}
+
+let spans = document.querySelectorAll(".spans");
+
+for (let index = 0; index < spans.length; index++) {
+    const element = spans[index];
+    element.addEventListener("click", (e) => {
+        let deletedClass = e.target.className.replace(" spans", "");
+        let element = document.querySelector("." + deletedClass);
+        element.remove();
+        let text = element.textContent.replace("done", "");
+        let newToDos = allToDos.replace(text, "").replace(" |", "");
+        localStorage.setItem("to-dos", newToDos);
+    })
+    
+}
+
 addButton.addEventListener("click", () => {
     addTask.classList.add("flex");
 })
@@ -15,13 +50,17 @@ addButton.addEventListener("click", () => {
 let todos;
 
 addTaskAddButton.addEventListener("click", () => {
-    if (localStorage.getItem("to-dos") == null) {
-       todos = "";
+    if(input.value != "") {
+        if (localStorage.getItem("to-dos") == null) {
+        todos = "";
+        } else {
+            todos = localStorage.getItem("to-dos")  + "|";
+        }
+        todoList.innerHTML += "<p>" + input.value + "<span>done</span>" + "</p>";
+        localStorage.setItem("to-dos", todos + input.value);
+        input.value = "";
+        addTask.classList.remove("flex");
     } else {
-        todos = localStorage.getItem("to-dos")  + ",";
+        alert("fill in input");
     }
-    todoList.innerHTML += "<p>" + input.value + "</p>";
-    localStorage.setItem("to-dos", todos + input.value);
-    input.value = "";
-    addTask.classList.remove("flex");
 })
